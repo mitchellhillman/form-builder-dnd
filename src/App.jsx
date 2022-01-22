c1f2fa335cc955a754f20851bfb94776411ea563
Mitchell Hillman <mitchell.hillman@bottomline.com>
2022-01-22 13:25:41 -0500

    parcel setup

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import * as classes from './App.module.css';
+
+export default () => <div className={classes.app}>
+  <h1 className={classes.title}>Form Builder</h1>
+</div>;
